2e2c5ac5551b9b1c5778babf8e2eb0d0ecd2228b
Evrim P <evrimfeyyaz@gmail.com>
2019-07-05 18:32:05 +0300

    Add a bottom separator to section container, adjust section container height to fit section title

--- src/components/containers/Section.tsx
+++ src/components/containers/Section.tsx
@@ -6,29 +6,46 @@ interface SectionProps {
 }
 
 export const Section: FunctionComponent<SectionProps> = ({ title, children = null }) => {
+  const MARGIN = 20;
+
   const [titleMarginStart, setTitleMarginStart] = useState(0);
+  const [titleTopPosition, setTitleTopPosition] = useState(0);
+  const [containerMinHeight, setContainerMinHeight] = useState(0);
 
-  /**
-   * Adjusts the margin of the container title to lay against
-   * the left side of the screen.
-   * @param e
-   */
-  const adjustMarginStart = (e: LayoutChangeEvent) => {
-    let { width, height } = e.nativeEvent.layout;
+  const moveTitleLeft = (titleWidth: number, titleHeight: number) => {
+    setTitleMarginStart(-(titleWidth / 2) + (titleHeight / 2) + MARGIN);
+  };
 
-    setTitleMarginStart(-(width / 2) + (height / 2) + 20);
+  const moveTitleIntoContainer = (titleWidth: number) => {
+    const offset = -6;
+
+    setTitleTopPosition(titleWidth / 2 + offset);
+  };
+
+  const fitTitleInContainer = (titleWidth: number) => {
+    setContainerMinHeight(titleWidth + MARGIN);
+  };
+
+  const handleTitleLayout = (e: LayoutChangeEvent) => {
+    const { width, height } = e.nativeEvent.layout;
+
+    moveTitleLeft(width, height);
+    moveTitleIntoContainer(width);
+    fitTitleInContainer(width);
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={StyleSheet.flatten([styles.title, { marginStart: titleMarginStart }])}
-            onLayout={adjustMarginStart}>
+    <View style={StyleSheet.flatten([styles.container, { minHeight: containerMinHeight }])}>
+      <Text style={StyleSheet.flatten([styles.title, { marginStart: titleMarginStart, top: titleTopPosition }])}
+            onLayout={handleTitleLayout}>
         {title}
       </Text>
 
       <View style={styles.content}>
         {children}
       </View>
+
+      <View style={styles.separator} />
     </View>
   );
 };
@@ -44,13 +61,22 @@ const styles = StyleSheet.create({
     fontSize: 12,
     left: 0,
     position: 'absolute',
-    textTransform: 'uppercase',
     transform: [{ rotate: '-90deg' }],
+    textTransform: 'uppercase',
     zIndex: 999,
   },
   content: {
     color: '#fff',
     flex: 1,
     paddingStart: 60,
+  },
+  separator: {
+    borderBottomColor: '#fff',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    opacity: .2,
+    bottom: 0,
+    right: 0,
+    width: '84%',
+    position: 'absolute',
   },
 });
