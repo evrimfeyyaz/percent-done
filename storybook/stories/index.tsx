e60c3ebd0aeceb39a39cec5fccbc361e9f73e7d2
Evrim P <evrimfeyyaz@gmail.com>
2019-07-08 10:48:36 +0300

    Add a number knob to the progress chart story

--- storybook/stories/index.tsx
+++ storybook/stories/index.tsx
@@ -3,8 +3,10 @@ import { storiesOf } from '@storybook/react-native';
 import CenterView from './CenterView';
 import { Body, ProgressChart, Section } from '../../src/components';
 import { addDecorator } from '@storybook/react-native/dist';
+import { withKnobs, number } from '@storybook/addon-knobs';
 
 addDecorator((getStory: any) => <CenterView>{getStory()}</CenterView>);
+addDecorator(withKnobs);
 
 storiesOf('Typography', module)
   .add('Body', () => (
@@ -19,6 +21,17 @@ storiesOf('Containers', module)
   ));
 
 storiesOf('Charts', module)
-  .add('Progress Chart', () => (
-    <ProgressChart percentDone={75} />
-  ));
+  .add('Progress Chart', () => {
+    const label = 'Percent Done';
+    const defaultValue = 75;
+    const options = {
+      range: true,
+      min: 0,
+      max: 100,
+      step: 1,
+    };
+
+    const percentDone = number(label, defaultValue, options);
+
+    return (<ProgressChart percentDone={percentDone} />);
+  });
