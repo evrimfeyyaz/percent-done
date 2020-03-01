915ee9875da2299181e9fdff3d9772cc301e48db
Evrim P <evrimfeyyaz@gmail.com>
2020-03-01 15:45:34 +0400

    Add a temporary fix for the issue that causes navigation bar titles to not be truncated until it is fixed in react-navigation-stack

--- src/navigators/headerConfig.ts
+++ src/navigators/headerConfig.ts
@@ -1,10 +1,15 @@
 import { colors, fonts } from '../theme';
 import { NavigationRoute, NavigationScreenConfig } from 'react-navigation';
 import { NavigationStackOptions, NavigationStackProp } from 'react-navigation-stack';
+import { Platform } from 'react-native';
 
 export const headerConfig: NavigationScreenConfig<NavigationStackOptions, NavigationStackProp<NavigationRoute, any>> = {
   headerStyle: {
     backgroundColor: colors.offWhite,
+  },
+  headerTitleContainerStyle: {
+    width: Platform.OS === 'ios' ? '60%' : '75%',
+    alignItems: Platform.OS === 'ios' ? 'center' : 'flex-start',
   },
   headerTitleStyle: {
     fontFamily: fonts.semibold,
